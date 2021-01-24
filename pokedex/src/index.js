cfec16d5c215b53903496c8f1a11c6fe42c0ba5f
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2021-01-24 15:25:33 -0600

    getting routes set up

--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import Store from "./Store"
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
--- pokedex/src/reducers/RootReducers.js
+++
@@ -1,4 +0,0 @@
-import {combineReducers} from "redux";
-const RootReducer = combineReducers(reducers:{
-    
-});
