30814c1c94581b2c71990b5081459258ff1f0bd9
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2021-02-11 11:57:47 -0600

    moved Fetch data

--- pokedex/src/containers/PokemonList.js
+++ pokedex/src/containers/PokemonList.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { useDispatch, useSelector} from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import _ from "lodash";
 import { GetPokemonList } from "../actions/pokemonActions";
 import { Link } from "react-router-dom";
@@ -11,18 +11,20 @@ const PokemonList = (props) => {
     const pokemonList = useSelector(state => state.PokemonList);
     React.useEffect(() => {
         FetchData(1)
-    }, []);
+    }, [FetchData]);
 
     const FetchData = (page = 1) => {
         dispatch(GetPokemonList(page))
 
     }
 
-    if (pokemonList.loading) {
-        return <p>Loading...</p>
-    }
+    
 
     const showData = () => {
+        if (pokemonList.loading) {
+            return <p>Loading...</p>
+        }
+
         if (!_.isEmpty(pokemonList.data)) {
             return (
                 <div className="list-wrapper">
@@ -37,8 +39,8 @@ const PokemonList = (props) => {
                 </div>
             )
         }
-        
-        
+
+
 
         if (pokemonList.errorMsg !== "") {
             return <p>{pokemonList.errorMsg}</p>
@@ -47,22 +49,22 @@ const PokemonList = (props) => {
     };
     return (
         <div>
-            <div className ="search-wrapper">
+            <div className="search-wrapper">
                 <p>Search</p>
-                <input type="text" onChange={e => setSearch(e.target.value)}/>
+                <input type="text" onChange={e => setSearch(e.target.value)} />
                 <button onClick={() => props.history.push(`/pokemon/${search}`)}>Search</button>
 
             </div>
             {showData()}
-           {!_.isEmpty(pokemonList.data) && (
-               <ReactPaginate 
-               pageCount={Math.ceil(pokemonList.count / 15)}
-               pageRangeDisplayed={2}
-               marginPagesDisplayed={1}
-               onPageChange={(data) => FetchData(data.selected + 1)}
-               containerClassName={"paginationgit"}
-               />
-           )}
+            {!_.isEmpty(pokemonList.data) && (
+                <ReactPaginate
+                    pageCount={Math.ceil(pokemonList.count / 15)}
+                    pageRangeDisplayed={2}
+                    marginPagesDisplayed={1}
+                    onPageChange={(data) => FetchData(data.selected + 1)}
+                    containerClassName={"pagination"}
+                />
+            )}
         </div>
     )
 };
